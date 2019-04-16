40231974c2933294091330f587433a75f79dca95
root <root@localhost.localdomain>
2019-04-16 16:16:24 +0200

    fichier pom corrigé packaging jar

--- src/test/java/com/wunderit/AppTest.java
+++ src/test/java/com/wunderit/AppTest.java
@@ -1,38 +1,24 @@
 package com.wunderit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest { 
+
+    @Test
+    public void test() throws Exception {int a = 1;
+    assertEquals(a, 1);}
+    
+    @Ignore @Test
+    public void test2() throws Exception {assertEquals(true, true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void test3() throws Exception {assertEquals(false, true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
